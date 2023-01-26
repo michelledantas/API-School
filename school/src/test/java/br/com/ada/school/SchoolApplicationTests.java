c78e2f9b8baf8332d81351480bfa232c618a1c04
Michelle <michelledantasluiz@gmail.com>
2023-01-25 21:21:51 -0300

    Criando o projeto

---
+++ school/src/test/java/br/com/ada/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ada.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
